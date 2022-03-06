32584089b9b68a03810140bf4a75f03008067e67
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-03-06 22:47:57 +0100

    fix the problem with movies image
    don't forget  return

--- movies/src/includes/Main/Main.js
+++ movies/src/includes/Main/Main.js
@@ -4,7 +4,6 @@ import './main.css';
 const APIURL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-let main=null;
 
 function getClassByRate(vote) {
   if (vote >= 8) {
@@ -21,42 +20,30 @@ function Main() {
   
   useEffect(()=>{
       axios.get(APIURL).then(res=>{
-        
-        const moviess=[res.data.results];
-        console.log(moviess);
-        setMovies(moviess);
+        setMovies(res.data.results);
       },[])
   },[])
   return (
     <div className='Main'>
-      { main= document.querySelector('.Main')}
-      
       {
        
         movies.map(movie=>{
           const { poster_path, title, vote_average, overview } = movie;
-
-          const movieEl = document.createElement("div");
-          movieEl.classList.add("movie");
-
-          movieEl.innerHTML = `
-              <img
-                  src="${IMGPATH + poster_path}"
-                  alt="${title}"
-              />
-              <div class="movie-info">
-                  <h3>${title}</h3>
-                  <span class="${getClassByRate(
+          return (
+          <div key={movie.id} class="movie">
+                  <img src={IMGPATH + poster_path} alt={title} />
+                  <div class="movie-info">
+                      <h3>{title}</h3>
+                      <span class={getClassByRate(
                       vote_average
-                  )}">${vote_average}</span>
-              </div>
-              <div class="overview">
-                  <h3>Overview:</h3>
-                  ${overview}
-              </div>
-          `;
-             main.appendChild(movieEl);
-             
+                      )}>{vote_average}</span>
+                  </div>
+                  <div class="overview">
+                      <h3>Overview:</h3>
+                      {overview}
+                  </div>
+            </div>
+          )
         })
       }
     </div>
